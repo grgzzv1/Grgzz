3e745f77992d07e838d9a677bdff6149a97333e7
Samel Grgzz <grgxpemba3@gmail.com>
2025-03-24 21:18:36 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import platform
 import os
-import socket
+import sys
+import platform
 import subprocess
 
 arc = None
@@ -11,18 +11,24 @@ os.system('git pull --quiet')
 def main():
     global arc
     architecture = platform.architecture()
+    
     if architecture[0] == '32bit':
         arc = "32BIT"
-        exit(f' •\x1b[38;5;196m ->\x1b[37m 32BIT NOT SUPPORTED')
+        sys.exit(f' •\x1b[38;5;196m ->\x1b[37m 32BIT NOT SUPPORTED')
+    
     elif architecture[0] == '64bit':
         arc = "64BIT"
         print(f' •\x1b[38;5;196m ->\x1b[37m 64BIT DETECTED')
         print(f' •\x1b[38;5;196m ->\x1b[37m STARTING Grgzz ')
-        import data.HARRY64
+        
+        try:
+            import data.HARRY64
+        except ImportError:
+            sys.exit("ERROR: data.HARRY64 module not found!")
+    
     else:
         arc = "INVALID"
-        exit("•\x1b[38;5;196m ->\x1b[37m UNKNOWN DEVICE TYPE")
-
+        sys.exit("•\x1b[38;5;196m ->\x1b[37m UNKNOWN DEVICE TYPE")
 
 if __name__ == "__main__":
     main()
